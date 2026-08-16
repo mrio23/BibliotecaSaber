0fbe6dd4e6842bdaeea8230f69d43f33fa6f68c9
mrio23 <mariofscampos@gmail.com>
2026-08-16 14:46:27 -0300

    Melhoria geral, para PCs e Celulares

--- database/connection.py
+++ database/connection.py
@@ -3,7 +3,10 @@ from sqlalchemy import create_engine
 
 DATABASE_URL = st.secrets["DATABASE_URL"]
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+    )
 
 def get_connection():
     return engine.connect()
